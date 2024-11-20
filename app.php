0742d1dbd8c4280433e568292dbdc3970bda45ab
rqhost <jimgon.zuc09@gmail.com>
2024-11-19 19:52:40 -0600

    Create app.php

---
+++ app.php
@@ -0,0 +1,431 @@
+<?php
+
+// Array con la información de los streams de video
+$streams = [
+    [
+        'title' => 'IMAGEN TV',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10822.ts',
+        'image' => 'https://brandemia.org/sites/default/files/sites/default/files/grupo_imagen_logo.jpg',
+        'category' => 'LOCALES MX',
+    ],
+    [
+        'title' => 'LAS ESTRELLAS',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10840.ts',
+        'image' => 'https://ugi.mx/web/image/3036-9c985ef0/Las%20Estrellas%20Nueva%20Gr%C3%A1fica.png',
+        'category' => 'LOCALES MX',
+    ],
+    [
+        'title' => 'EL NU9VE',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/154124.ts',
+        'image' => 'https://directostv.teleame.com/wp-content/uploads/2019/10/El-Nueve-Mexico-en-vivo-Online-Teleame-Mexico.png',
+        'category' => 'LOCALES MX',
+    ],
+    [
+        'title' => 'AZTECA 7',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2092601.ts',
+        'image' => 'https://cdn.mitvstatic.com/channels/mx_azteca-7_m.png',
+        'category' => 'LOCALES MX',
+    ],
+    [
+        'title' => 'CANAL 5',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2092598.ts',
+        'image' => 'http://ceoapps.org/logos/canal5.png',
+        'category' => 'LOCALES MX',
+    ],
+    [
+        'title' => 'ATV PERU',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/577886.m3u8',
+        'image' => 'https://i0.wp.com/directostv.teleame.com/wp-content/uploads/2020/04/ATV-Per%C3%BA-en-vivo-Online.png',
+        'category' => 'LATINOS',
+    ],
+    [
+        'title' => 'ATV PERU OP2',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/585247.m3u8',
+        'image' => 'https://i0.wp.com/directostv.teleame.com/wp-content/uploads/2020/04/ATV-Per%C3%BA-en-vivo-Online.png',
+        'category' => 'LATINOS',
+    ],
+    [
+        'title' => 'CARACOL',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531531.m3u8',
+        'image' => 'https://cdn.mitvstatic.com/channels/co_caracol_m.png',
+        'category' => 'LATINOS',
+    ],
+    [
+        'title' => 'CARACOL OP2',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531533.m3u8',
+        'image' => 'https://cdn.mitvstatic.com/channels/co_caracol_m.png',
+        'category' => 'LATINOS',
+    ],
+    [
+        'title' => 'CANAL RCN',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531536.m3u8',
+        'image' => 'https://cdn.mitvstatic.com/channels/60ef29f0-d7fb-487d-85c8-a1fbcf2ff8da_m.jpg',
+        'category' => 'LATINOS',
+    ],
+    [
+        'title' => 'CANAL RCN OP2',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531537.m3u8',
+        'image' => 'https://cdn.mitvstatic.com/channels/60ef29f0-d7fb-487d-85c8-a1fbcf2ff8da_m.jpg',
+        'category' => 'LATINOS',
+    ],
+    [
+        'title' => 'AMERICA TV',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/577878.m3u8',
+        'image' => 'https://www.tvyvideo.com/images/stories/TVV/2021/America_Television_mejora_su_produccion_con_Ross_Video.jpg',
+        'category' => 'LATINOS',
+    ],
+    [
+        'title' => 'AMERICA TV OP2',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/577879.m3u8',
+        'image' => 'https://www.tvyvideo.com/images/stories/TVV/2021/America_Television_mejora_su_produccion_con_Ross_Video.jpg',
+        'category' => 'LATINOS',
+    ],
+    [
+        'title' => 'TNT SERIES',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2147953.ts',
+        'image' => 'https://static.epg.best/mx/TNTSeries.mx.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'LIFE TIME',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2147951.ts',
+        'image' => 'https://static.epg.best/mx/Lifetime.mx.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'TELEMUNDO INTERNACIONAL',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2145673.ts',
+        'image' => 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh3Z3WNRcnuhTxMOlZtslC2MAwymXrWLNNgi-_LHvFmxlavSH6_taHFqfXqLKVIKpDJUVzZOc2NsVbXG-JIZnNkrQxdStUBocGNL3nJsE-GTeZJYkA66LWkD_iA79me5t1F3EceZStySek/s340/TelemundoInternacional-2018.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'AYE',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/222250.ts',
+        'image' => 'https://static.epg.best/mx/AandE.mx.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'UNIVERSAL TV',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/197619.ts',
+        'image' => 'https://i0.wp.com/infomarketing.pe/wp-content/uploads/2018/10/universal-tv-900.jpg',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'TL NOVELAS',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/154093.ts',
+        'image' => 'https://conexion360.mx/wp-content/uploads/2020/03/foto-Tlnovelas.jpg',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'WARNER',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10583.ts',
+        'image' => 'https://static.epg.best/ar/Warner.ar.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'UNICABLE',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10582.ts',
+        'image' => 'http://ceoapps.org/logos/unicable.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'SONY MOVIES',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10577.ts',
+        'image' => 'https://revista.produ.com/wp-content/uploads/2022/03/SonyMovies701X367P4.jpg',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'STAR CHANNEL',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10558.ts',
+        'image' => 'http://ceoapps.org/logos/starchannel.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'DISTRITO COMEDIA',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10554.ts',
+        'image' => 'https://static.epg.best/mx/DistritoComedia.mx.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'BITME',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10551.ts',
+        'image' => 'https://www.newslinereport.com/online/nota_bitme-sera-el-nuevo-canal-de-videojuegos-esports-y-anime-de-televisa.jpg',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'AXN',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10549.ts',
+        'image' => 'https://prnoticias.com/wp-content/uploads/2016/10/k2_items_src_8e6c0ce7ce800a9ab9a0fced676c0c0c.jpg',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'AMC',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10547.ts',
+        'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/AMC_logo_2019.svg/1200px-AMC_logo_2019.svg.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'FX',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/148725.ts',
+        'image' => 'https://variety.com/wp-content/uploads/2024/07/FX.jpg',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'STUDIO UNIVERSAL',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2147954.ts',
+        'image' => 'https://cdn.mitvstatic.com/channels/mx_studio-universal_m.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'CLARO CINEMA',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2091457.ts',
+        'image' => 'https://uspto.report/TM/86830508/mark.png',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'UNIVERSAL CINEMA',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2081448.ts',
+        'image' => 'https://i.ibb.co/7g8X2f4/da40ee40-1c40-4874-ac33-75e7cc66bf15-1.jpg',
+        'category' => 'SERIES Y PELICULAS',
+    ],
+    [
+        'title' => 'UNIVERSAL PREMIERE',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2081446.ts',
+        'image' => 'https://i.ibb.co/3TDQVsN/35ff32e4-cc09-4929-a6ec-c8094402fac2.jpg',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'SPACE',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/193851.ts',
+        'image' => 'https://cdn.mitvstatic.com/channels/co_space-hd_m.png',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'TCM',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/187786.ts',
+        'image' => 'https://brandemia.org/sites/default/files/sites/default/files/logo_tcm.jpg',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'TNT',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/155176.ts',
+        'image' => 'https://cdn.mitvstatic.com/channels/mx_tnt-hd_m.png',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'HBO MUNDI',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/155167.ts',
+        'image' => 'https://static.wikia.nocookie.net/rarter/images/c/cd/Mundi.png',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'DE PELICULA',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/154098.ts',
+        'image' => 'https://i.ibb.co/NnLqsq0/imgpsh-fullsize-anim.jpg',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'HBO POP',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/41421.ts',
+        'image' => 'https://static.wikia.nocookie.net/newlogosfake/images/f/ff/HBOPopLA2020.png',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'HBO',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10886.ts',
+        'image' => 'https://i0.wp.com/prensaescenario.com/wp-content/uploads/2020/09/image001-16.jpg',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'HBO 2',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10884.ts',
+        'image' => 'https://logowik.com/content/uploads/images/hbo-2-hd-nl3042.logowik.com.webp',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'HBO FAMILY',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10878.ts',
+        'image' => 'IMAGEN',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'PARAMOUNT CHANNEL',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10867.ts',
+        'image' => 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/HBO_Family_Asia_logo.svg/200px-HBO_Family_Asia_logo.svg.png',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'GOLDEN EDGE',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10859.ts',
+        'image' => 'https://cdn.eldeforma.com/wp-content/uploads/2020/09/Cover-Clases-Televisio%CC%81n-Golden-Edge-Anatomi%CC%81a.jpg',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'CINEMAX',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10847.ts',
+        'image' => 'https://www.totalmedios.com/img/noticias/2017/07/5979d65311fbc__838x390.jpg',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'SPACE',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/155173.ts',
+        'image' => 'https://cdn.mitvstatic.com/channels/mx_space_m.png',
+        'category' => 'PELICULAS',
+    ],
+    [
+        'title' => 'ESPN DEPORTES',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2218544.ts',
+        'image' => 'https://i.ibb.co/mb2HPTT/1.jpg',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'ESPN DEPORTES OP2',
+        'url' => 'http://etvhome.com:25461/live/restreamplus/demoswnce/35073.ts',
+        'image' => 'https://i.ibb.co/mb2HPTT/1.jpg',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'TVC DEPORTES',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2145706.ts',
+        'image' => 'https://img2.sport-tv-guide.live/images/tv-station-tvc-deportes-946.png',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'FOX SPORTS PREMIUM',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2106730.ts',
+        'image' => 'https://i.blogs.es/61a41c/fox-sports-premium-230322-1200x675-1/1366_2000.jpg',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'TUDN USA',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/194924.ts',
+        'image' => 'https://www.newscaststudio.com/wp-content/uploads/2019/05/tudn-logo.jpg',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'TUDN',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/191356.ts',
+        'image' => 'http://startv.solutions:80/images/QCqX5p5x3Y181Q8jwb82JLz-jtq8r_ZWcXHf_4b_eSHP7LdsmGfWz_2PhiQK5bim.png',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'TUDN MX',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/155675.ts',
+        'image' => 'http://startv.solutions:80/images/QCqX5p5x3Y181Q8jwb82JLz-jtq8r_ZWcXHf_4b_eSHP7LdsmGfWz_2PhiQK5bim.png',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'FOX SPORTS',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/155648.ts',
+        'image' => 'https://i.ibb.co/tpyvjLz/0588199a-94fd-4541-a873-452a5fd15d31.jpg',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'ESPN',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/155472.ts',
+        'image' => 'https://i.imgur.com/r5kftbo.png',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'ESPN 2',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/155473.ts',
+        'image' => 'http://startv.solutions:80/images/QCqX5p5x3Y181Q8jwb82JH6D17JoD_K7VPek2h92mXsDijUJsGl461YmN590LrAd.png',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'FOX SPORTS 2',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/10949.ts',
+        'image' => 'http://startv.solutions:80/images/QCqX5p5x3Y181Q8jwb82JML2YXawmfQ1ps4eFV_qpLOG2F_idIYQ1nHujjqqZg7r.png',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'FOX DEPORTES',
+        'url' => 'http://sdmx.vip:80/live/Daleplayperu52/tVFeXytHnZ/2216927.ts',
+        'image' => 'https://images.pluto.tv/channels/63d0269d60bc8f000890facc/thumbnail.jpg',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'TYC SPORTS',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/411419.m3u8',
+        'image' => 'https://images.pluto.tv/channels/63d0269d60bc8f000890facc/thumbnail.jpg',
+        'category' => 'DEPORTES',
+    ],
+    [
+        'title' => 'ANIMAL PLANET',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531661.m3u8',
+        'image' => 'https://i.revistapym.com.co/old/2019/02/animal-planet-1.jpg',
+        'category' => 'CULTURA',
+    ],
+    [
+        'title' => 'DISCOVERY HYH',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531679.m3u8',
+        'image' => 'https://cdn.mitvstatic.com/channels/b0d27efc-174d-41f6-851b-e125fba0dce7_m.jpg',
+        'category' => 'CULTURA',
+    ],
+    [
+        'title' => 'DISCOVERY ID',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531680.m3u8',
+        'image' => 'https://cdn.mitvstatic.com/channels/co_discovery-id-hd_m.png',
+        'category' => 'CULTURA',
+    ],
+    [
+        'title' => 'HISTORY 2',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531702.m3u8',
+        'image' => 'https://static.wikia.nocookie.net/logopedia/images/7/78/H2.png',
+        'category' => 'CULTURA',
+    ],
+    [
+        'title' => 'NATGEO',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/531716.m3u8',
+        'image' => 'https://noticiasibo.com/wp-content/uploads/2017/11/natgeo.jpg',
+        'category' => 'CULTURA',
+    ],
+    [
+        'title' => 'DISNEY CHANNEL',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/372319.m3u8',
+        'image' => 'https://seeklogo.com/images/D/Disney_Channel-logo-5143178965-seeklogo.com.png',
+        'category' => 'CARICATURAS',
+    ],
+    [
+        'title' => 'CARTOON NETWORK',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/372325.m3u8',
+        'image' => 'https://seeklogo.com/images/C/Cartoon_Network-logo-20456E3678-seeklogo.com.png',
+        'category' => 'CARICATURAS',
+    ],
+    [
+        'title' => 'CARTOONITO',
+        'url' => 'https://zonamovie.live:8443/live/Tvmagica2/9DTgBtZMxV/441881.m3u8',
+        'image' => 'https://cdn.milenio.com/uploads/media/2021/12/01/cartoonito-reempla-a-boomerang-especial.jpg',
+        'category' => 'CARICATURAS',
+    ],
+];
+
+// User-Agent esperado de Roku (puede variar según el modelo)
+$rokuUserAgent = 'Roku/DVP-9.1 (9.1.0-4210X; TCL Roku TV)'; 
+
+// Obtener el User-Agent de la solicitud
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+// Verificar si el User-Agent es compatible con Roku
+if (strpos($userAgent, 'Roku/') === 0) { 
+
+    // Cabecera M3U
+    header('Content-Type: application/vnd.apple.mpegurl');
+    header('Content-Disposition: attachment; filename="lista_roku.m3u"');
+
+    // Escribir la cabecera M3U
+    echo "#EXTM3U\n";
+
+// Recorrer el array de streams y generar las entradas de la lista
+foreach ($streams as $stream) {
+    echo "#EXTINF:-1 tvg-logo=\"" . $stream['image'] . "\" group-title=\"" . $stream['category'] . "\"," . $stream['title'] . "\n";
+    echo $stream['url'] . "\n";
+}
+
+} else {
+    // Mostrar un mensaje de error si el User-Agent no es de Roku
+    http_response_code(403); // Forbidden
+    echo "";
+}
+
+?>
